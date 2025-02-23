892b2c33444e266f0e62309f55c42ec50e710ca7
rikkin-the <k74rikkin@outlook.com>
2025-02-24 05:38:56 +0900

    Introduce database into todo list

--- screens/MorningRoutine.js
+++ screens/MorningRoutine.js
@@ -1,35 +1,114 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { CheckBox } from 'react-native-elements'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import * as SQLite from 'expo-sqlite';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
-const data = [1, 2]
 
 export default function MorningRoutine() {
-  
+  const [text, setText] = useState('');
+  const [items, setItems] = useState([]);
 
-  const [text, setText] = useState('')
-  const [items, setItems] = useState(data)
-  const [checked, setChecks] = useState(Array(data.length).fill(false));
-  
+
+  const insertItem = (str) => {
+    setItems(prev => {
+      const newArray = [...prev, {task: str, checked: false}];
+      return newArray;
+    });
+  };
 
   const toggleCheckBox = index => {
-    setChecks(prev => {
-      const newChecked = [...prev];
-      newChecked[index] =!newChecked[index];
-      return newChecked;
-    })
-  }
+    setItems(prev => {
+      const newArray = [...prev];
+      newArray[index].checked =!newArray[index].checked;
+      return newArray;
+    });
+  };
 
   const createNewItem = str => {
-    if(text.trim() === '') return;
-    setItems([...items, str]);
+    if(str.trim() === '') return;
+    insertItem(str);
     setText('');
   }
 
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+    setItems(prev => {
+      const newArray = [...prev];
+      return newArray.filter(obj => obj.task !== rowKey);
+    });
+  };
 
+  const saveRecord = () => {
+    const taskNum = items.length;
+    let achievedNum = 0;
+    const listForSave = [];
+    for(let i = 0; i < taskNum; i++) {
+      if(items[i].checked) achievedNum++;
+      listForSave.push(items[i].task);
+    }
+    const achievement = 100*(achievedNum/taskNum);
+    const today = new Date().toISOString().split('T')[0];
+
+    const saveList = async () => {
+      try {
+        const jsonList = JSON.stringify(listForSave);
+        await AsyncStorage.removeItem('todoList');
+        await AsyncStorage.setItem('todoList', jsonList);
+      } catch(error) {
+        console.log('AsyncStorageに挿入するときのエラー', error);
+      }
+    }
+
+    const saveAchievement = async () => {
+      try {
+        const db = await SQLite.openDatabaseAsync('example.db'); //ファイル名検討
+        await db.execAsync(`
+          PRAGMA jounal_mode = WAL;
+
+          CREATE TABLE IF NOT EXISTS progress (
+            id INTEGER PRIMARY KEY NOT NULL,
+            date TEXT NOT NULL,
+            achievement INTEGER 
+          );    
+        `)
+        await db.runAsync('INSERT INTO progress (date, achievement) values (?, ?)', today, Math.floor(achievement));
+      } catch(error) {
+        console.log('データ挿入のエラー', error);
+      }
+    }
+
+    alert(`${Math.floor(achievement)}%タスク達成で朝活を終えます。よろしいですか？`);
+    saveList();
+    saveAchievement();
+    
+  }
+
+  useEffect(() => {
+    const getPrevList = async () => {
+      try {
+        const prevList = await AsyncStorage.getItem('todoList');
+        if(prevList !== null) { 
+          const arrList = JSON.parse(prevList);
+          if(arrList) {
+            for(let i = 0; i < arrList.length; i++) {
+              insertItem(arrList[i]);
+            }
+          }
+        }
+      } catch(error) {
+        console.error("テーブル取得または作成に失敗:", error);
+      }
+    };
+
+    getPrevList();
+  }, [])
+
+  
   return (
    <SafeAreaProvider>
       <SafeAreaView style={styles.containerWhole}>
@@ -43,21 +122,6 @@ export default function MorningRoutine() {
           <View style={[styles.row, {flex: 10}]}>
             <View style={styles.containerTodo}>
               <Text style={styles.title}>今日の朝活ToDo</Text>
-              <KeyboardAwareScrollView behavior="padding" style={styles.list}>
-                <FlatList
-                  style={styles.list}
-                  data={items}
-                  renderItem={({item, index}) => (
-                    <View style={styles.itemBlock}>
-                      <CheckBox
-                        style={styles.checkbox}
-                        checked={checked[index]}
-                        onPress={() => toggleCheckBox(index)}
-                      />
-                      <Text style={styles.text}>{item}</Text>
-                    </View>
-                  )}
-                />
                 <TextInput
                     style={styles.input}
                     value={text}
@@ -65,11 +129,38 @@ export default function MorningRoutine() {
                     placeholder='todoを追加'
                     onSubmitEditing={() => createNewItem(text)}
                 />
-              </KeyboardAwareScrollView>
+                <SwipeListView
+                  disableRightSwipe
+                  rightOpenValue={-65}
+                  style={styles.list}
+                  keyExtractor={(item, index) => item.task}
+                  data={items}
+                  renderItem={({item, index}) => (
+                    <View style={styles.frontBlock}>
+                      <CheckBox
+                        style={styles.checkbox}
+                        checked={item.checked}
+                        onPress={() => toggleCheckBox(index)}
+                      />
+                      <Text style={styles.item}>{item.task}</Text>
+                    </View>
+                  )}
+                  renderHiddenItem={(data, rowMap) => (
+                    <View style={styles.backBlock}>
+                      <TouchableOpacity 
+                        style={styles.delBtn}
+                        onPress={() => {closeRow(rowMap, data.item.task)}}
+                      >
+                        <Text style={styles.delText}>削除</Text>
+                      </TouchableOpacity>
+                    </View> 
+                  )}
+               
+                />
             </View>
           </View>
-          <View style={[styles.row, {flex: 2}]}>
-            <Button title='朝活を終える' />
+          <View style={[styles.row, {flex: 2, padding: 24}]}>
+            <Button title='朝活を終える' onPress={() => {saveRecord()} }/>
           </View>
         </View>
       </SafeAreaView>
@@ -107,20 +198,38 @@ const styles = StyleSheet.create({
     width: '95%',
     margin: 16,
   },
-  itemBlock: {
+  frontBlock: {
     flexDirection: 'row',
     alignItems: 'center',
+    backgroundColor: 'pink',
+    borderBottomColor: 'black',
+    borderBottomWidth: 0.3,
+    height: 50,
   },
-  text: {
-    fontSize: 24,
+  item: {
+    fontSize: 20,
     flexShrink: 1,
+  },
+  backBlock: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    flex: 1,
+  },
+  delBtn: {
+    flex: 1,
+  },
+  delText: {
+    marginLeft: 'auto',
+    padding: 16,
+    fontSize: 16,
+    color: 'white',
   },
   input: {
     backgroundColor: 'white',
-    padding: 16,
+    padding: 12,
     width: '90%',
     borderRadius: 8,
     fontSize: 16,
-    margin: 16,
   }
 });
